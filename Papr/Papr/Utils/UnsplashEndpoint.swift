5d0958f7333a4837bd471211c684f563a446ebf2
Joan Disho <dishojoan@gmail.com>
2017-11-02 23:24:30 +0100

    configure Moya.

--- Papr/Papr/Utils/UnsplashEndpoint.swift
+++ Papr/Papr/Utils/UnsplashEndpoint.swift
@@ -97,6 +97,46 @@ enum Unsplash {
     
 }
 
+enum UnsplashScope {
+
+    case pub
+    case readUser
+    case writeUser
+    case readPhotos
+    case writePhotos
+    case writeLikes
+    case writeFollowers
+    case readCollections
+    case writeCollections
+    case fullScope
+    
+    var value: String {
+        switch self {
+        case .pub:
+            return "public"
+        case .readUser:
+            return "read_user"
+        case .writeUser:
+            return "write_user" 
+        case .readPhotos:
+            return "read_photos"
+        case .writePhotos:
+            return "read_photos"
+        case .writeLikes:
+            return "write_likes"
+        case .writeFollowers:
+            return "write_followers"
+        case .readCollections:
+            return "read_collections"
+        case .writeCollections:
+            return "write_collections"
+        case .fullScope:
+            return "public read_user write_user read_photos write_photos write_likes write_followers read_collections write_collections"
+        }
+        
+    }
+}
+
 extension Unsplash: TargetType {
 
     var baseURL: URL {
@@ -253,6 +293,10 @@ extension Unsplash: TargetType {
     
     var headers: [String : String]? {
         return nil
+    }
+    
+     var validate: Bool { 
+        return true
     }
     
 }
