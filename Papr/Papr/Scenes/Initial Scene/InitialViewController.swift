624d37c82348004b802b6faa95ee2f1e2cc1b429
Joan Disho <dishojoan@gmail.com>
2017-11-12 00:11:40 +0100

    create RxMoya (#4)

--- Papr/Papr/Scenes/Initial Scene/InitialViewController.swift
+++ Papr/Papr/Scenes/Initial Scene/InitialViewController.swift
@@ -38,18 +38,6 @@ class InitialViewController: UIViewController, BindableType {
         sessionManager.retrier = oauthHandler
          
         provider = MoyaProvider<Unsplash>(manager: sessionManager)
-        
-        provider.request(.searchCollections("tech", 1, 10)) { (result) in
-            if case .success(let response) = result {
-                do {
-                    let a = try response.map(to: CollectionsResult.self)
-                    print(a)
-                } catch let error {
-                    print("There was something wrong with the request! Error: \(error)")
-                }
-            }
-        }
-
     }
 
 }
