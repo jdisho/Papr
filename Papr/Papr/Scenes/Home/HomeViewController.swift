59879b28ae92d0e01871860d2a12bb530d190721
Joan Disho <dishojoan@gmail.com>
2018-02-23 02:08:55 +0100

    order photos at home by frequecy, popularity and switch between curated and new photos.

--- Papr/Papr/Scenes/Home/HomeViewController.swift
+++ Papr/Papr/Scenes/Home/HomeViewController.swift
@@ -24,9 +24,10 @@ class HomeViewController: UIViewController, BindableType {
     @IBOutlet var tableView: UITableView!
     
     // MARK: Private
-
     private var dataSource: RxTableViewSectionedReloadDataSource<HomeSectionModel>!
     private var refreshControl: UIRefreshControl!
+    private var navBarButton: UIButton!
+    private var rightBarButtonItem: UIBarButtonItem!
 
     // MARK: Override
 
@@ -42,15 +43,53 @@ class HomeViewController: UIViewController, BindableType {
     // MARK: BindableType
     
     func bindViewModel() {
+        let inputs = viewModel.inputs
+        let outputs = viewModel.outputs
+
         dataSource = RxTableViewSectionedReloadDataSource<HomeSectionModel>(
             configureCell:  tableViewDataSource
         )
+
+        outputs.curated.subscribe { [unowned self] curated in
+            guard let curated = curated.element else { return }
+            self.resetTableViewPosition()
+            if curated {
+                 self.navBarButton.rx.action = inputs.showLatestPhotosAction
+            } else {
+                 self.navBarButton.rx.action = inputs.showCuratedPhotosAction
+            }
+        }
+        .disposed(by: rx.disposeBag)
         
-        viewModel.outputs.isRefreshing
+        outputs.orderBy.subscribe { [unowned self] orderBy in
+            guard let orderBy = orderBy.element else { return }
+            self.resetTableViewPosition()
+            switch orderBy {
+            case .latest:
+                self.rightBarButtonItem.rx.action = inputs.orderByPopularityAction
+            case .popular:
+                self.rightBarButtonItem.rx.action = inputs.orderByFrequencyAction
+            case .oldest: 
+                self.rightBarButtonItem.rx.action = nil
+            }
+        }
+        .disposed(by: rx.disposeBag)
+
+        outputs.orderBy
+            .map { $0 == .popular ? #imageLiteral(resourceName: "hot") : #imageLiteral(resourceName: "new")}
+            .bind(to: rightBarButtonItem.rx.image)
+            .disposed(by: rx.disposeBag)
+
+        outputs.isRefreshing
             .bind(to: refreshControl.rx.isRefreshing)
             .disposed(by: rx.disposeBag)
-        
-        viewModel.outputs.photos
+
+        outputs.navBarButtonName
+            .map { $0.string }
+            .bind(to: navBarButton.rx.title())
+            .disposed(by: rx.disposeBag)
+
+        outputs.photos
             .map { [SectionModel(model: "", items: $0)] }
             .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: rx.disposeBag)
@@ -58,20 +97,26 @@ class HomeViewController: UIViewController, BindableType {
         tableView.rx
             .contentOffset
             .flatMap { [unowned self] _ in 
-                Observable.just(self.tableView.isNearTheBottomEdge())
+                return Observable.just(self.tableView.isNearTheBottomEdge())
             }
             .distinctUntilChanged()
-            .skipUntil(viewModel.outputs.isRefreshing)
-            .bind(to: viewModel.inputs.loadMore)
+            .skipUntil(outputs.isRefreshing)
+            .bind(to: inputs.loadMore)
             .disposed(by: rx.disposeBag)
     }
 
     // MARK: UI
     
     private func configureNavigationController() {
-        self.title = "Home 🏡"
+        navBarButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        navBarButton.setTitleColor(.black, for: .normal)
+        navBarButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        rightBarButtonItem = UIBarButtonItem()
+        navigationItem.titleView = navBarButton
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        
     }
-    
+
     private func configureTableView() {
         tableView.registerCell(type: HomeViewCell.self)
         tableView.estimatedRowHeight = 400
@@ -81,6 +126,10 @@ class HomeViewController: UIViewController, BindableType {
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
         tableView.addSubview(refreshControl)
+    }
+    
+    private func resetTableViewPosition() {
+        tableView.setContentOffset(CGPoint(x: 0, y: -150), animated: true)
     }
     
     @objc private func refresh() {
---
+++ Papr/Papr/Utils/Extentions/Rx/UIBarButtonItem+Rx.swift
@@ -0,0 +1,22 @@
+//
+//  UIBarButtonItem+Rx.swift
+//  Papr
+//
+//  Created by Joan Disho on 23.02.18.
+//  Copyright © 2018 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UIBarButtonItem {
+    
+    /// Bindable sink for `image` property.
+    public var image: Binder<UIImage> {
+        return Binder(self.base) { button, image in
+            button.image = image
+        }
+    }
+
+}
