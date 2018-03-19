78b0f4ebf4262f3900c16ee5be38c27b1bed9f85
Joan Disho <dishojoan@gmail.com>
2018-03-19 13:44:11 +0100

    use env variables for clientID and secret

--- Papr/Papr/Utils/Enums/Unsplash/UnsplashSettings.swift
+++ Papr/Papr/Utils/Enums/Unsplash/UnsplashSettings.swift
@@ -24,15 +24,31 @@ enum UnsplashSettings {
         case .callbackURLScheme:
             return "papr://"
         case .clientID:
-            return "0dd1dc721b7672e96f4bdf71af623fb68ea7b1d4829681744a4a08e0a8ef68c8"
+            return UnsplashSecrets.clientID
         case .clientSecret:
-            return "550dfea297d53e54b05f0a59458a3bddce6217f8f44e34d30524a3b0d2425eca"
+            return UnsplashSecrets.clientSecret
         case .authorizeURL:
             return "https://unsplash.com/oauth/authorize"
         case .tokenURL:
             return "https://unsplash.com/oauth/token"
         case .redirectURL:
             return "papr://unsplash"
+        }
+    }
+
+    enum UnsplashSecrets {
+
+        static let clientID = UnsplashSecrets.environmentVariable(named: "UNSPLASH_CLIENT_ID") ?? ""
+        static let clientSecret = UnsplashSecrets.environmentVariable(named: "UNSPLASH_CLIENT_SECRET") ?? ""
+
+        static func environmentVariable(named: String) -> String? {
+            let processInfo = ProcessInfo.processInfo
+            guard let value = processInfo.environment[named] else {
+                print("‼️ Missing Environment Variable: '\(named)'")
+                return nil
+            }
+            return value
+
         }
     }
 }
