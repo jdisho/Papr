6af7d4d869601ab65a1ccb05a9dffbddc687cf5c
Joan Disho <dishojoan@gmail.com>
2018-04-16 15:52:25 +0200

    double tap to zoom in/out photo

--- Papr/Scenes/Photo Details/PhotoDetailsViewController.swift
+++ Papr/Scenes/Photo Details/PhotoDetailsViewController.swift
@@ -17,10 +17,7 @@ class PhotoDetailsViewController: UIViewController, BindableType {
     var viewModel: PhotoDetailsViewModelType!
 
     // MARK: IBOutlets
-    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var dismissButton: UIButton!
-    @IBOutlet var photoImageView: UIImageView!
-    @IBOutlet var photoHeightConstraint: NSLayoutConstraint!
     @IBOutlet var totalViewsLabel: UILabel!
     @IBOutlet var totalLikesLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
@@ -35,17 +32,25 @@ class PhotoDetailsViewController: UIViewController, BindableType {
     private static let nukeManager = Nuke.Manager.shared
     private let disposeBag = DisposeBag()
     private var isTouched = true
+    private var scrollView: UIScrollView!
+    private var photoImageView: UIImageView!
+    private var tapGesture: UITapGestureRecognizer!
+    private var doubleTapGesture: UITapGestureRecognizer!
 
     // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configureUI()
+        configureAll()
         showHideOverlays(withDelay: 0.5)
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+
+    override func viewWillLayoutSubviews() {
+        centerScrollViewIfNeeded()
     }
 
      // MARK: BindableType
@@ -68,7 +73,10 @@ class PhotoDetailsViewController: UIViewController, BindableType {
 
         outputs.photoSizeCoef
             .map { CGFloat($0) }
-            .bind(to: photoHeightConstraint.rx.constant)
+            .subscribe { result in
+                guard let height = result.element else { return }
+                self.configureContentSize(withHeight: height)
+            }
             .disposed(by: disposeBag)
 
         outputs.likedByUser
@@ -104,30 +112,181 @@ class PhotoDetailsViewController: UIViewController, BindableType {
     }
 
     // MARK: UI
-    private func configureUI() {
-        let tapGesture = UITapGestureRecognizer(
-            target: self,
-            action: #selector(showHideOverlays))
-        view.addGestureRecognizer(tapGesture)
+    private func configureAll() {
+        configureScrollView()
+        configurePhotoImageView()
+        configureTapGestures()
+
         view.bringSubview(toFront: dismissButton)
         view.bringSubview(toFront: statsContainerView)
     }
 
+    private func configureTapGestures() {
+        tapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(showHideOverlays(withDelay:)))
+
+        tapGesture.addTarget(self,
+            action: #selector(zoomOut))
+
+        doubleTapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(zoomInOut(gestureRecognizer:)))
+
+        doubleTapGesture.addTarget(self,
+            action: #selector(hideOverlays))
+
+        tapGesture.numberOfTapsRequired = 1
+        doubleTapGesture.numberOfTapsRequired = 2
+
+        tapGesture.delegate = self
+        doubleTapGesture.delegate = self
+
+        view.addGestureRecognizer(tapGesture)
+        view.addGestureRecognizer(doubleTapGesture)
+    }
+
+    private func configureScrollView() {
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.delegate = self
+        scrollView.maximumZoomScale = 3
+        scrollView.contentMode = .center
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        view.addSubview(scrollView)
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        scrollView.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor,
+            constant: 0).isActive = true
+
+        scrollView.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor,
+            constant: 0).isActive = true
+
+        scrollView.topAnchor.constraint(
+            equalTo: view.topAnchor,
+            constant: 0).isActive = true
+
+        scrollView.bottomAnchor.constraint(
+            equalTo: view.bottomAnchor,
+            constant: 0).isActive = true
+    }
+
+    private func configurePhotoImageView() {
+        photoImageView = UIImageView(frame: view.bounds)
+        photoImageView.contentMode = .scaleAspectFit
+        scrollView.addSubview(photoImageView)
+    }
+
+    private func configureContentSize(withHeight height: CGFloat) {
+        let bounds = view.bounds
+        scrollView.frame = bounds
+        let size: CGSize
+        let containerSize = CGSize(width: bounds.width, height: bounds.height)
+        if containerSize.width / containerSize.height < view.bounds.size.width / height {
+            size = CGSize(
+                width: containerSize.width,
+                height: containerSize.width * height / view.bounds.size.width
+            )
+        } else {
+            size = CGSize(
+                width: containerSize.height * view.bounds.size.width / height,
+                height: containerSize.height
+            )
+        }
+        photoImageView.frame = CGRect(origin: .zero, size: size)
+        scrollView.contentSize = size
+    }
+
+
+    private func zoomRect(forScale scale: CGFloat, withCenter center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = photoImageView.frame.size.height / scale
+        zoomRect.size.width  = photoImageView.frame.size.width  / scale
+        let newCenter = photoImageView.convert(center, from: scrollView)
+        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
+        return zoomRect
+    }
+
+    func centerScrollViewIfNeeded() {
+        var inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        if scrollView.contentSize.height < scrollView.bounds.height {
+            let insetV = (scrollView.bounds.height - scrollView.contentSize.height)/2
+            inset.top += insetV
+            inset.bottom = insetV
+        }
+        if scrollView.contentSize.width < scrollView.bounds.width {
+            let insetV = (scrollView.bounds.width - scrollView.contentSize.width)/2
+            inset.left = insetV
+            inset.right = insetV
+        }
+        scrollView.contentInset = inset
+    }
+
     @objc private func showHideOverlays(withDelay delay: Double = 0.0) {
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-            UIView.animate(withDuration: 0.3,
-                           animations: {
+            UIView.animate(withDuration: 0.3, animations: { [unowned self] in
                 if self.isTouched {
                     self.statsContainerViewBottomConstraint.constant = 0
                     self.dismissButtonTopConstraint.constant = 32
                     self.isTouched = false
                 } else {
-                    self.statsContainerViewBottomConstraint.constant = 132
-                    self.dismissButtonTopConstraint.constant = -100
-                    self.isTouched = true
+                    self.hideOverlays()
                 }
                 self.view.layoutIfNeeded()
             })
         }
+    }
+
+    @objc private func hideOverlays() {
+        self.statsContainerViewBottomConstraint.constant = 132
+        self.dismissButtonTopConstraint.constant = -100
+        self.isTouched = true
+    }
+
+
+
+    @objc func zoomInOut(gestureRecognizer: UITapGestureRecognizer) {
+        if scrollView.zoomScale == 1 {
+            scrollView.zoom(
+                to: zoomRect(
+                    forScale: scrollView.maximumZoomScale,
+                    withCenter: gestureRecognizer.location(in: gestureRecognizer.view)),
+                animated: true)
+        } else {
+            zoomOut()
+        }
+    }
+
+    @objc func zoomOut() {
+        scrollView.setZoomScale(1, animated: true)
+    }
+
+}
+
+// MARK: UIScrollViewDelegate
+
+extension PhotoDetailsViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoImageView
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerScrollViewIfNeeded()
+    }
+}
+
+// MARK: PhotoDetailsViewController
+
+extension PhotoDetailsViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer
+        ) -> Bool {
+        return gestureRecognizer == self.tapGesture &&
+            otherGestureRecognizer == self.doubleTapGesture
     }
 }
