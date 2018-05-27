fc3844f2c6547909ef582d259daaac77f3a30aa7
Joan Disho <dishojoan@gmail.com>
2018-05-27 19:34:11 +0200

    configure collection view

--- Papr/Scenes/Search/SearchPhotos/SearchPhotosViewController.swift
+++ Papr/Scenes/Search/SearchPhotos/SearchPhotosViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxDataSources
 
 class SearchPhotosViewController: UIViewController, BindableType {
+
+    typealias SearchPhotosSectionModel = SectionModel<String, SearchPhotosCellModelType>
 
     // MARK: ViewModel
     var viewModel: SearchPhotosViewModelType!
@@ -18,12 +21,40 @@ class SearchPhotosViewController: UIViewController, BindableType {
     // MARK: IBOutlets
     @IBOutlet var collectionView: UICollectionView!
 
+    // MARK: Privates
+    private var dataSource: RxCollectionViewSectionedReloadDataSource<SearchPhotosSectionModel>!
+    private let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureCollectionView()
     }
 
     func bindViewModel() {
         
+    }
+
+    // MARK: UI
+    private func configureCollectionView() {
+        collectionView.registerCell(type: SearchPhotosCell.self)
+        guard let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        flowLayout.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        flowLayout.itemSize = CGSize(width: 150, height: 150)
+
+        dataSource = RxCollectionViewSectionedReloadDataSource<SearchPhotosSectionModel>(
+            configureCell:  collectionViewDataSource
+        )
+    }
+
+    private var collectionViewDataSource: CollectionViewSectionedDataSource<SearchPhotosSectionModel>.ConfigureCell {
+        return { _, collectionView, indexPath, cellModel in
+            var cell = collectionView.dequeueReusableCell(
+                type: SearchPhotosCell.self,
+                forIndexPath: indexPath)
+
+            return cell
+        }
     }
 }
 
