c76363e62c597fdb53036293369bb1e1cfe98a24
Joan Disho <dishojoan@gmail.com>
2018-09-26 14:38:57 +0200

    show a loading view while searched users are not yet shown

---
+++ Papr/Scenes/LoadingView/LoadingView.swift
@@ -0,0 +1,38 @@
+//
+//  LoadingView.swift
+//  Papr
+//
+//  Created by Joan Disho on 18.09.18.
+//  Copyright © 2018 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import VanillaConstraints
+
+class LoadingView: UIView {
+
+    private let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        backgroundColor = .white
+
+        activityIndicatorView
+            .add(to: self)
+            .size(CGSize(width: 30, height: 30))
+            .center()
+
+        activityIndicatorView.startAnimating()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func stopAnimating() {
+        activityIndicatorView.stopAnimating()
+        isHidden = true
+    }
+}
