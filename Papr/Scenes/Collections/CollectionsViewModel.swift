10b5037d0309fd26f6af7acfbef144e845ab2113
Joan Disho <dishojoan@gmail.com>
2018-10-02 11:06:20 +0200

    add pagination logic

--- Papr/Scenes/Collections/CollectionsViewModel.swift
+++ Papr/Scenes/Collections/CollectionsViewModel.swift
@@ -9,8 +9,18 @@
 import Foundation
 import RxSwift
 
-protocol CollectionsViewModelInput {}
+protocol CollectionsViewModelInput {
+    /// Call when the bottom of the list is reached
+    var loadMore: BehaviorSubject<Bool> { get }
+
+    /// Call when pull-to-refresh is invoked
+    func refresh()
+}
 protocol CollectionsViewModelOutput {
+    /// Emits a boolean when the pull-to-refresh control is refreshing or not.
+    var isRefreshing: Observable<Bool>! { get }
+
+    /// Emites the child viewModels
     var collectionCellsModelType: Observable<[CollectionCellViewModelType]> { get }
 }
 protocol CollectionsViewModelType {
@@ -26,8 +36,15 @@ class CollectionsViewModel: CollectionsViewModelType,
     var output: CollectionsViewModelOutput { return self }
 
     // MARK: Inputs
+    let loadMore = BehaviorSubject<Bool>(value: false)
+
+    func refresh() {
+        refreshProperty.onNext(true)
+    }
 
     // MARK: Outputs
+    var isRefreshing: Observable<Bool>!
+
     lazy var collectionCellsModelType: Observable<[CollectionCellViewModelType]> = {
         return photoCollections.mapMany { CollectionCellViewModel(photoCollection: $0) }
     }()
@@ -35,25 +52,66 @@ class CollectionsViewModel: CollectionsViewModelType,
     // MARK: Private
     private let service: CollectionServiceType
     private let sceneCoordinator: SceneCoordinatorType
-    private let photoCollections: Observable<[PhotoCollection]>
+    private var photoCollections: Observable<[PhotoCollection]>!
+    private let refreshProperty = BehaviorSubject<Bool>(value: true)
 
     // MARK: Init
-    init(service: CollectionServiceType =
-        CollectionService(),
+    init(service: CollectionServiceType = CollectionService(),
          sceneCoordinator: SceneCoordinatorType = SceneCoordinator.shared) {
 
         self.service = service
         self.sceneCoordinator = sceneCoordinator
 
-        photoCollections = service
-            .collections(byPageNumber: 1, curated: false)
-            .flatMap { result -> Observable<[PhotoCollection]> in
-                switch result {
-                case let .success(photoCollections):
-                    return Observable.just(photoCollections)
-                case .error(_):
-                    return Observable.empty()
+        var currentPageNumber = 1
+        var collectionArray = [PhotoCollection]([])
+
+        isRefreshing = refreshProperty.asObservable()
+
+        let requestFirst = isRefreshing
+            .flatMapLatest { isRefreshing -> Observable<[PhotoCollection]> in
+                guard isRefreshing else { return .empty() }
+                return service
+                    .collections(byPageNumber: currentPageNumber, curated: false)
+                    .flatMap { [unowned self] result -> Observable<[PhotoCollection]> in
+                        switch result {
+                        case let .success(photoCollections):
+                            return Observable.just(photoCollections)
+                        case .error(_):
+                            self.refreshProperty.onNext(false)
+                            return Observable.empty()
+                        }
                 }
             }
+            .do (onNext: { _ in
+                collectionArray = []
+                currentPageNumber = 1
+            })
+
+        let requestNext = loadMore.asObservable()
+            .flatMapLatest { isLoadingMore -> Observable<[PhotoCollection]> in
+                guard isLoadingMore else { return .empty() }
+                currentPageNumber += 1
+                return service
+                    .collections(byPageNumber: currentPageNumber, curated: false)
+                    .flatMap { [unowned self] result -> Observable<[PhotoCollection]> in
+                        switch result {
+                        case let .success(photoCollections):
+                            return Observable.just(photoCollections)
+                        case .error(_):
+                            self.refreshProperty.onNext(false)
+                            return Observable.empty()
+                        }
+                }
+            }
+
+        photoCollections = requestFirst
+            .merge(with: requestNext)
+            .map { [unowned self] collections -> [PhotoCollection] in
+                collections.forEach { collection in
+                    collectionArray.append(collection)
+                }
+                self.refreshProperty.onNext(false)
+                return collectionArray
+        }
     }
 }
