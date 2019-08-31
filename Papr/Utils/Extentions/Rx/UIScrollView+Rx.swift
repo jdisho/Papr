e52d33159061b40e9f9e6f63d7ddb04ae9d933df
Joan Disho <dishojoan@gmail.com>
2019-08-31 22:46:46 +0200

    make reachesBottom a ControlEvent

--- Papr/Utils/Extentions/Rx/UIScrollView+Rx.swift
+++ Papr/Utils/Extentions/Rx/UIScrollView+Rx.swift
@@ -10,20 +10,21 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-extension Reactive where Base: UIScrollView {
-    func reachedBottom(withOffset offset: CGFloat = 0.0) -> Observable<Bool> {
-        let observable = contentOffset
-            .map { [weak base] contentOffset -> Bool in
-                guard let scrollView = base else { return false}
-                let visibleHeight = scrollView.frame.height
-                    - scrollView.contentInset.top
-                    - scrollView.contentInset.bottom
-                let y = contentOffset.y + scrollView.contentInset.top
-                let threshold = max(offset, scrollView.contentSize.height - visibleHeight)
-
-                return y > threshold
+public extension Reactive where Base: UIScrollView {
+    /**
+     Shows if the bottom of the UIScrollView is reached.
+     - parameter offset: A threshhold indicating the bottom of the UIScrollView.
+     - returns: ControlEvent that emits when the bottom of the base UIScrollView is reached.
+     */
+    func reachedBottom(offset: CGFloat = 500.0) -> ControlEvent<Bool> {
+        let source = contentOffset.map { contentOffset -> Bool in
+            let visibleHeight = self.base.frame.height - self.base.contentInset.top - self.base.contentInset.bottom
+            let y = contentOffset.y + self.base.contentInset.top + self.base.contentInset.bottom
+            let threshold = max(offset, self.base.contentSize.height - visibleHeight - offset)
+            return y >= threshold
         }
+        .distinctUntilChanged()
 
-        return observable.distinctUntilChanged()
+        return ControlEvent(events: source)
     }
 }
